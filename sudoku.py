4a31a7382a96a4be8f786f242f0cc9dca8ee2c15
jswoo <wnstjd4852@gmail.com>
2022-07-11 22:21:11 +0900

    스도쿠 박스 컨투어 진행 중

--- sudoku.py
+++ sudoku.py
@@ -6,21 +6,32 @@ img1 = "/Users/jswoo/Desktop/Repository/OpenCV_Sudoku/1.png"
 img2 = "/Users/jswoo/Desktop/Repository/OpenCV_Sudoku/2.png"
 
 def BoxSearch(img):
-    img = cv2.imread(img)
+    img_ori = cv2.imread(img)
+    img = img_ori.copy()
+    
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,5))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, vertical_kernel, iterations=9)
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, horizontal_kernel, iterations=4)
 
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(img_gray, 50, 150, apertureSize = 3)
+    thresh = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,57,5)
+
     ## 선으로 대우를 받고 싶으면 최소길이가 100이어야하고 라인사이 갭은 10이어야한다.
     # p가 붙어서 확률 적으로 무작위로 허프변환 해본다 
     # probabilistic
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=20, maxLineGap=10)
+    lines2 = cv2.HoughLinesP(thresh, 1, np.pi/180, 100, minLineLength=40, maxLineGap=10)
 
-    for line in lines:
+    for line in lines2:
         ## 리스트에서 1,2,3,4, 씩 계속 뽑아옴
         x1, y1, x2, y2 = line[0]
         cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 2)
 
-    cv2.imshow('test',img)
+    cv2.imshow('ori',img_ori)
+    cv2.imshow('test', img)
+    cv2.imshow('test1', thresh)
     cv2.imshow('edges',edges)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
